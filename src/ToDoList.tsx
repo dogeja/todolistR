65ae93d9a0178f9fc1593311a3341b8e42d0ccb4
dogeja <disk6473@gmail.com>
2023-02-14 23:25:50 +0900

    0214

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -26,18 +26,56 @@ import { useForm } from "react-hook-form";
     </div>
   );
 }; */
+interface IFormData {
+  [key: string]: string;
+}
 const ToDolist = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <div>
-      <form>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         {/* 객체 내부의 값을 꺼내   스프레드함 */}
-        <input {...register("Email")} placeholder="Write a Todo"></input>
-        <input {...register("FirstName")} placeholder="FirstName"></input>
-        <input {...register("LastName")} placeholder="LastName"></input>
-        <input {...register("ID")} placeholder="ID"></input>
-        <input {...register("PW")} placeholder="PW"></input>
+        <input
+          {...register("Email", {
+            required: "입력해줘 제발",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+              message: "올바른 형식으로 입력해 주세요",
+            },
+          })}
+          placeholder="Write a Todo"
+        ></input>
+        <span style={{ color: "white" }}>{errors?.Email?.message}</span>
+        <input
+          {...register("FirstName", { required: true })}
+          placeholder="FirstName"
+        ></input>
+        <input
+          {...register("LastName", { required: true })}
+          placeholder="LastName"
+        ></input>
+        <input
+          {...register("ID", {
+            required: true,
+            minLength: {
+              value: 6,
+              message: "6글자 이상으로 입력해주세요!",
+            },
+          })}
+          placeholder="ID"
+        ></input>
+        <input {...register("PW", { required: true })} placeholder="PW"></input>
         <button>ADD</button>
       </form>
     </div>
